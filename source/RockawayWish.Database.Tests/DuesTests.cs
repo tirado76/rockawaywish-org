38ecbc8489bdfaa25bcf8123fdbabf4944cd2eab
Richie <richie@nightagency.com>
2016-05-16 21:58:58 -0400

    Create database tests project

---
+++ source/RockawayWish.Database.Tests/DuesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RockawayWish.Database.Repositories;
+
+namespace RockawayWish.Database.Tests
+{
+    [TestClass]
+    public class DuesTests
+    {
+        [TestMethod]
+        public void InsertDues()
+        {
+        }
+
+        [TestMethod]
+        public void UpdateDues()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteDues()
+        {
+        }
+        
+        [TestMethod]
+        public void InsertUserDues()
+        {
+        }
+
+        [TestMethod]
+        public void UpdateUserDues()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteUserDues()
+        {
+        }
+    }
+}
