5f196a14324fbc70e108fc959147c6bb0f95bc8b
Richie <richie@nightagency.com>
2016-05-16 21:11:31 -0400

    Create database project and entity framework

---
+++ source/RockawayWish.Database/Repositories/DuesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockawayWish.Database;
+
+namespace RockawayWish.Database.Repositories
+{
+    public class DuesRepository
+    {
+        public DuesRepository()
+        {
+            _DBContext = new RockawayWishDBContext();
+        }
+        private RockawayWishDBContext _DBContext;
+    }
+}
---
+++ source/RockawayWish.Database/Repositories/IDuesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockawayWish.Database;
+
+namespace RockawayWish.Database.Repositories
+{
+    public interface IDuesRepository
+    {
+        int InsertDues(int duesYear, decimal duesAmount);
+        int UpdateDues(int DuesId, int duesYear, decimal duesAmount);
+        int DeleteDues(int DuesId);
+    }
+}
